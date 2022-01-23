a99265726d6b629e918db0cf791ac75fbfd4f9e4
sujay.purayil <sujay.purayil@yokogawa.com>
2022-01-23 12:38:45 +0800

    create 'temp' if not exist

--- download.py
+++ download.py
@@ -9,19 +9,19 @@ import os
 class Downloader:
     def __init__(self) -> None:
         logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-        self._url = sys.argv[1]
-        self._id = 1        
+        self._url = sys.argv[1]        
         pass
     
     def _download_fn(self,id):
         logging.info("Thread %s: starting", id)
+        if not os.path.exists('temp'):
+            os.makedirs('temp')
         res = urllib.request.urlretrieve(self._url, f'temp/temp{id}.mp4') 
         os.remove(f'temp/temp{id}.mp4')
         logging.info(f'downloaded {self._url}: {res}')
         logging.info("Thread %s: finishing", id)
 
-    def download(self,id):
-        self._id = id
+    def download(self,id):        
         x = threading.Thread(target=self._download_fn,args=(id,), daemon=True)        
         x.start()
         return x
