7c15ef5090c0c04d6753e3b7de64eb3a6b857e0f
sujay.purayil <sujay.purayil@yokogawa.com>
2022-01-23 12:13:32 +0800

    first commit

---
+++ download.py
@@ -0,0 +1,41 @@
+from distutils import log
+import urllib.request
+import threading
+import logging
+import time
+import sys
+import os       
+
+class Downloader:
+    def __init__(self) -> None:
+        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+        self._url = sys.argv[1]
+        self._id = 1        
+        pass
+    
+    def _download_fn(self,id):
+        logging.info("Thread %s: starting", id)
+        res = urllib.request.urlretrieve(self._url, f'temp/temp{id}.mp4') 
+        os.remove(f'temp/temp{id}.mp4')
+        logging.info(f'downloaded {self._url}: {res}')
+        logging.info("Thread %s: finishing", id)
+
+    def download(self,id):
+        self._id = id
+        x = threading.Thread(target=self._download_fn,args=(id,), daemon=True)        
+        x.start()
+        return x
+
+if __name__=="__main__":
+    dl = Downloader()
+    threads = list()
+    for i in range(1,int(sys.argv[2])+1):
+        threads.append(dl.download(f'req_{i}'))
+    logging.info(f'{sys.argv[2]} started')
+    for index, thread in enumerate(threads):
+        # logging.info("Main    : before joining thread %d.", index)
+        thread.join()        
+        logging.info("Main    : thread %d done", index)   
+    
+
+
